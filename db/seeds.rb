d1651d2c6e9490359f6a83bc9c555899e3bf8f88
Emily Boucher <emi.boucher@gmail.com>
2017-10-20 01:20:05 -0400

    add model, seed objects, added all to index view thru controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Picture.create(
+  title:    'Art History is Not Linear',
+  artist:   'Ryan McGinness',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2014/03/McGinness-slider-crop-700x450.jpg'
+)
+Picture.create(
+  title:    'Stadia III',
+  artist:   'Julie Mehretu',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2013/12/2006-1_v1_XL-1024x781.jpg'
+)
+Picture.create(
+  title:    'Green Wave',
+  artist:   'Inka Essenhigh',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2013/12/2003-4_CS_5-2007_XL-1024x852.jpg'
+)
+Picture.create(
+  title:    '#06-32,  Carrara',
+  artist:   'William Wylie',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2013/12/VMFA_2007-2_v1_kw_x-1024x816.jpg'
+)
+Picture.create(
+  title:    'Direct Object 14',
+  artist:   'Jon-Phillip Sheridan',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2013/12/VMFA_2012-6_Sheridan_Direct-Object-14_x-811x1024.jpg'
+)
+Picture.create(
+  title:    'The Former and the Ladder',
+  artist:   'Trenton Doyle Hancock',
+  url:      'http://magazine.art21.org/wp-content/uploads/2013/11/1.jpg'
+)
+Picture.create(
+  title:    'Sailfishe',
+  artist:   'James Prosek',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2014/01/Sailfishe400x400.jpeg'
+)
+Picture.create(
+  title:    'Untitled (Yellow Corner)',
+  artist:   'Richard Roth',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2013/12/VMFA_2013-5_Roth_x-1015x1024.jpg'
+)
+Picture.create(
+  title:    'Blue Husk',
+  artist:   'Tony Oursler',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2013/12/VMFA_2001-223_cs_x-1024x805.jpg'
+)
+Picture.create(
+  title:    'The Prig',
+  artist:   'Kristin Baker',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2014/03/VMFA-The-Prig-by-Kristin-Baker-1007x1024.jpg'
+)
+
+Picture.create(
+  title:    'Untitled (Tower)',
+  artist:   'Mark Fox',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2013/12/VMFA_2011-525_v1_TF_x-768x1024.jpg'
+)
+
+Picture.create(
+  title:    'Farwellscape',
+  artist:   'Jiha Moon',
+  url:      'https://vmfa.museum/collections/wp-content/uploads/sites/9/2013/12/VMFA_2007-14_v1_kw_X-549x1024.jpg'
+)
